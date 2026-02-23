f10d9d93895091282f5bb72487bbec3e6cc94be9
ryan <hardran3@gmail.com>
2026-02-23 02:10:42 -0700

    unified card rendering

---
+++ app/src/main/java/com/ryans/nostrshare/ui/ContentSegment.kt
@@ -0,0 +1,13 @@
+package com.ryans.nostrshare.ui
+
+import com.ryans.nostrshare.MediaUploadState
+import com.ryans.nostrshare.utils.LinkMetadata
+import org.json.JSONObject
+
+sealed class ContentSegment {
+    data class Text(val text: String) : ContentSegment()
+    data class MediaGroup(val items: List<MediaUploadState>) : ContentSegment()
+    data class LinkPreview(val meta: LinkMetadata) : ContentSegment()
+    data class NostrPreview(val event: JSONObject) : ContentSegment()
+    data class NostrLink(val bech32: String) : ContentSegment()
+}
